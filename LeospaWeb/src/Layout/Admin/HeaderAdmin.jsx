c3f8c4d38b56b15b0e2324295023e3d1cee002c5
Emil Blichfeldt <eb@limecode.dk>
2024-02-08 14:51:07 +0100

    commit "update"

--- LeospaWeb/src/Layout/Admin/HeaderAdmin.jsx
+++ LeospaWeb/src/Layout/Admin/HeaderAdmin.jsx
@@ -1,54 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { RxHamburgerMenu } from "react-icons/rx";
+import NavbarDesktop from '../Nav/Admin/NavbarDesktopAdmin';
+import NavbarMobile from '../Nav/Admin/NavbarMobileAdmin';
 
 const HeaderAdmin = () => {
+  const [navbar, setNavbar] = useState(false)
+  const isSmallScreen = window.innerWidth <= 425;
+  const handleNavbarClose = () => {
+    setNavbar(false)
+  };
   return (
     <>
-      <>
-        <header className="header">
-          <div className="logo">
-            <Link to={'/'}>
-              <img src="/public/assets/logo.png" alt="" />
-            </Link>
+      <header className="header mobileHeader">
+        <div className="logo">
+          <Link to={'/'}>
+            <img src="/public/assets/logo.png" alt="" />
+          </Link>
+        </div>
+        {isSmallScreen ? (
+          <div onClick={() => setNavbar(!navbar)}
+            className='navIcon'>
+            <RxHamburgerMenu />
           </div>
-          <nav className='navbar'>
-            <ul>
-              <ul>
-                <li>
-                  <Link to={'/admin'}>
-                    Dashboard
-                  </Link>
-                </li>
-                <li>
-                  <Link to={'/admin/treatment'}>
-                    Treatment
-                  </Link>
-                </li>
-                <li>
-                  <Link to={'/admin/hero'}>
-                    Hero
-                  </Link>
-                </li>
-                <li>
-                  <Link to={'about'}>
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link to={'footer/edit'}>
-                    Footer
-                  </Link>
-                </li>
-                <li>
-                  <Link to={'/'}>
-                    Logout
-                  </Link>
-                </li>
-              </ul>
-            </ul>
-          </nav>
-        </header>
-      </>
+        ) : (
+          <NavbarDesktop />
+        )}
+        <div className={`nav-container ${navbar ? 'open' : 'closed'}`}>
+          {isSmallScreen ? <NavbarMobile onCloseNav={handleNavbarClose} /> : null}
+        </div>
+      </header>
     </>
   )
 }
---
+++ LeospaWeb/src/Layout/Nav/Admin/NavbarDesktopAdmin.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const NavLink = [
+  { text: "Dashboard", to: "admin" },
+  { text: "Treatment", to: "/treatment" },
+  { text: "Hero", to: "/admin/hero" },
+  { text: "About", to: "about" },
+  { text: "Footer", to: "footer/edit" },
+  { text: "Log out", to: "/" }
+];
+
+const NavbarDesktop = () => {
+  return (
+    <>
+      <nav className='navbar'>
+        <ul>
+          {NavLink.map((item, index) => (
+            <li key={index}>
+              <Link to={item.to}>
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default NavbarDesktop
